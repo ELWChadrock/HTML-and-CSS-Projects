3e2354d377bf123fc0e8ee151bc690d5c54204c8
ELWChadrock <68598620+ELWChadrock@users.noreply.github.com>
2020-07-25 13:47:36 -0600

    Add files via upload

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<script>
+function My_First_Fuction() {
+var String = "Kiss me, I'm Irish!";
+var result = String.fontcolor("green");
+document.getElementById("Irish").innerhtml =
+result;
+}
+</script>
+</html>
